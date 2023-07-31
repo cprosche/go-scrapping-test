2b5f00877892a644d5979f1cb34cc91778e1a907
“Cade <caderosche@gmail.com>
2023-07-31 06:48:25 -0500

    working

--- main.go
+++ main.go
@@ -1,5 +1,21 @@
 package main
 
+import (
+	"log"
+
+	"github.com/gocolly/colly"
+)
+
 func main() {
-	println("Hello, World!")
+	c := colly.NewCollector()
+
+	c.OnHTML("a.js_link h2", func(e *colly.HTMLElement) {
+		title := e.Text
+		link, exists := e.DOM.Parent().Attr("href")
+		if exists {
+			log.Println(title + " (" + link + ")")
+		}
+	})
+
+	c.Visit("https://qz.com/latest")
 }
